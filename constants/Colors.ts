7f901f901c3ea239ae1c833557049610d778860a
Caio Bernardo <caiobernardo.dev@outlook.com>
2025-10-28 00:16:11 -0300

    chore: initial setup

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,15 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const colors = {
+  green: '#4A7C59',
+  black: '#1E1B18',
+  yellow: '#D9BB62',
+  beige: '#F2EDE4',
+  orangeLight: '#D98C4A',
+  orangeDark: '#D9653B',
+  red: '#D93829',
 
-export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
+  gradient: {
+    warm: ['#D9BB62', '#D9653B'] as const,
   },
 };
+
+export default colors;
