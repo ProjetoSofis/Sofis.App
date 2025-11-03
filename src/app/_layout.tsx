f610dc8b909d5dedd720e999ed35a5035b31b77e
Caio Bernardo <caiobernardo.dev@outlook.com>
2025-11-03 16:43:26 -0300

    feat: initial setup
    - fonts added;
    - created constants;
    - unnecessary files removed.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,21 +4,13 @@ import { Dimensions, StyleSheet, View } from "react-native";
 
 export default function MainLayout() {
   const { height } = Dimensions.get("window");
-  const paddingTop = height * 0.05; // 5% da altura da tela
+  const paddingTop = height * 0.05;
 
   return (
     <View style={[styles.container]}>
       <Stack>
         <Stack.Screen 
           name="index" 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="(auth)/signup/page" 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="(panel)/profile/page" 
           options={{ headerShown: false }} 
         />
       </Stack>
